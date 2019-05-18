7d90c86932aa648d368e4244cd3f78ce1ff0f555
hoonha <sosqsz2@naver.com>
2019-05-19 02:02:36 +0900

    업로드폴더 설정 , FBV 리스트 디테일 구현

--- photo/admin.py
+++ photo/admin.py
@@ -9,8 +9,7 @@ class PhotoAdmin(admin.ModelAdmin):
     raw_id_fields = ['author'] #ForeignKey 또는 ManyToManyField에 대한 입력 위젯으로 변경하려는 필드의 목록
     list_filter = ['created', 'updated', 'author']
     search_fields = ['text', 'created']
-    ordering = ['-updated']
-
+    ordering = ['-updated'] #모델 기본 정렬값이 아닌 어드민사이트 기본 사용될 정렬값
 
 
 admin.site.register(Photo, PhotoAdmin)
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 
 # Create your views here.
+from .models import Photo
+from django.views import generic
+
+def photo_list(request):
+    photos = Photo.objects.all()
+    return render(request, 'photo/index.html', {'photos': photos})
+
+def photo_detail(request, pk):
+    photos = get_object_or_404(Photo, pk=pk)
+    return render(request, 'shop/photo_detail.html', {'photos':photos})
+
