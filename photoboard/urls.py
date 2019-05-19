6117a43a8e4948e7058e3107afcbdc01babeec9c
hoonha <sosqsz2@naver.com>
2019-05-19 12:36:56 +0900

    이미지 url 설정

--- photoboard/urls.py
+++ photoboard/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('photo/', include('photo.urls')),
 ]
+
+#이미지 루트 설정
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
