969fc4c2baf671a3e8413cbc358ac46df73c2b48
hoonha <sosqsz2@naver.com>
2019-05-20 14:31:45 +0900

    폼 생성 및 게시판 생성

--- photo/urls.py
+++ photo/urls.py
@@ -5,4 +5,6 @@ app_name = 'photo'
 urlpatterns = [
     path('',  views.photo_list, name='list'),
     path('<int:pk>',  views.photo_detail, name='detail'),
+    path('create/', views.photo_create, name='create'),
+
 ]
--- photo/views.py
+++ photo/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render ,get_object_or_404
+from django.shortcuts import render ,get_object_or_404,redirect
 
 # Create your views here.
 from .models import Photo
 from django.views import generic
+from .forms import PhotoForm
 
 def photo_list(request):
     photos = Photo.objects.all()
@@ -10,5 +11,17 @@ def photo_list(request):
 
 def photo_detail(request, pk):
     photos = get_object_or_404(Photo, pk=pk)
-    return render(request, 'shop/photo_detail.html', {'photos':photos})
+    return render(request, 'photo/photo_detail.html', {'photos':photos})
 
+
+def photo_create(request):
+    if request.method == 'POST':
+        form = PhotoForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('photo:list')
+    else:
+        form = PhotoForm()
+    return render(request, 'photo/photo_create.html', {
+        'form': form,
+    })
